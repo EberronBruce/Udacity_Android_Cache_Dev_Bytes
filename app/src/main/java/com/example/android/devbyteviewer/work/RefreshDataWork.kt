fa1e32ecac10c04e2009dad492b796689cb73646
Bruce <bruce@redravencomputing.com>
2021-03-26 03:28:22 -0600

    Used working in app Application file

--- app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -26,6 +26,11 @@ import com.example.android.devbyteviewer.repository.VideosRepository
 
 
 class RefreshDataWork(appContext: Context, params: WorkerParameters) : CoroutineWorker(appContext, params) {
+
+    companion object {
+        const val WORK_NAME = "RefreshDataWorker"
+    }
+
     override suspend fun doWork(): Result {
         val database = getDatabase(applicationContext)
         val repository = VideosRepository(database)
